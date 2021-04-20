fb6c6c9096e81ba066046c9ebcbe92c8d6658b92
Klaudiusz Skowroński <claude-plos@o2.pl>
2021-04-20 15:14:08 +0200

    post mapping

--- src/main/java/pl/kskowronski/pattern_ractive_dev/api/StudentApi.java
+++ src/main/java/pl/kskowronski/pattern_ractive_dev/api/StudentApi.java
@@ -2,9 +2,12 @@ package pl.kskowronski.pattern_ractive_dev.api;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import pl.kskowronski.pattern_ractive_dev.models.Student;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
@@ -23,6 +26,15 @@ public class StudentApi {
 
     @GetMapping(produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
     public Flux<Student> getStudent() {
-        return studentFlux.delayElements(Duration.ofSeconds(2));
+        return studentFlux.delayElements(Duration.ofSeconds(1));
     }
+
+    @PostMapping(produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flux<Student> getStudent(@RequestBody Student student) {
+        studentFlux = this.studentFlux.mergeWith(Mono.just(student));
+        return studentFlux;
+    }
+
+
+
 }
